40046e294ef6c545e6e1a694f4bba7d3bf373b20
yashs9131 <yashs9131@gmail.com>
2022-06-10 00:31:14 +0530

    modified HTML and CSS

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
   } 
   changeEmail = (event) => {
     this.setState({ email: event.target.value });
-  } 
+  }
   submitButton = (event) => {
     event.preventDefault();
     if (this.state.firstName && this.state.email && this.state.lastName) {
@@ -35,28 +35,30 @@ class App extends React.Component {
       <div className='container'>
         { this.state.submitted && this.state.valid ? <div className='success-message'>Success! Thank you for registering</div> : null }
         <form className='row g-3' onSubmit={this.submitButton}>
-          <div class="row mb-3">
-            <label for="inputFirstName" class="col-sm-2 col-form-label-lg">First Name</label>
-            <div class="col-sm-10">
+          <div className="row mb-3">
+            <label htmlFor="inputFirstName" className="col-sm-2 col-form-label-lg">First Name</label>
+            <div className="col-sm-10">
               <input type="text" onChange={this.changeFirstName} value={this.state.firstName} className='form-control' placeholder='eg. John' name='firstName'/>
             </div>
             {this.state.submitted && !(this.state.firstName) ? <span>Please enter a first name</span> : null}
           </div>
-          <div class="row mb-3">
-            <label for="inputLastName" class="col-sm-2 col-form-label-lg">Last Name</label>
-            <div class="col-sm-10">
+          <div className="row mb-3">
+            <label htmlFor="inputLastName" className="col-sm-2 col-form-label-lg">Last Name</label>
+            <div className="col-sm-10">
               <input type="text" onChange={this.changeLastName} value={this.state.lastName} className='form-control' placeholder='eg. Krasinski' name='lastName'/>
             </div>
             {this.state.submitted && !(this.state.lastName) ? <span>Please enter a last name</span> : null}
           </div>
-          <div class="row mb-3">
-            <label for="inputEmail" class="col-sm-2 col-form-label-lg">Email</label>
-            <div class="col-sm-10">
+          <div className="row mb-3">
+            <label htmlFor="inputEmail" className="col-sm-2 col-form-label-lg">Email</label>
+            <div className="col-sm-10">
               <input type="email" onChange={this.changeEmail} value={this.state.email} className='form-control' placeholder='eg. johnkrasinski@gmail.com' name='email'/>
             </div>
             {this.state.submitted && !(this.state.email) ? <span>Please enter an email address</span> : null}
           </div>
-          <button type='submit' className="col-10">Register</button>
+          <div className="button-space">
+            <button type='submit' id='button'>Register</button>
+          </div>
         </form>
       </div>
     );
