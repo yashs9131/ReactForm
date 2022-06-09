ee00da20c03dd99fcd56efd86a172d18313058a7
yashs9131 <yashs9131@gmail.com>
2022-06-09 22:34:33 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -34,16 +34,30 @@ class App extends React.Component {
     return (
       <div className='container'>
         { this.state.submitted && this.state.valid ? <div className='success-message'>Success! Thank you for registering</div> : null }
-        <form className='register-form' onSubmit={this.submitButton}>
-          <input onChange={this.changeFirstName} value={this.state.firstName} className='form-field' placeholder='First Name' name='firstName'/>
-          {this.state.submitted && !(this.state.firstName) ? <span>Please enter a first name</span> : null}
-          <input onChange={this.changeLastName} value={this.state.lastName} className='form-field' placeholder='Last Name' name='lastName'/>
-          {this.state.submitted && !(this.state.lastName) ? <span>Please enter a last name</span> : null}
-          <input onChange={this.changeEmail} value={this.state.email} className='form-field' placeholder='Email' name='email'/>
-          {this.state.submitted && !(this.state.email) ? <span>Please enter an email address</span> : null}
-          <button>Register</button>
+        <form className='row g-3' onSubmit={this.submitButton}>
+          <div class="row mb-3">
+            <label for="inputFirstName" class="col-sm-2 col-form-label-lg">First Name</label>
+            <div class="col-sm-10">
+              <input type="text" onChange={this.changeFirstName} value={this.state.firstName} className='form-control' placeholder='eg. John' name='firstName'/>
+            </div>
+            {this.state.submitted && !(this.state.firstName) ? <span>Please enter a first name</span> : null}
+          </div>
+          <div class="row mb-3">
+            <label for="inputLastName" class="col-sm-2 col-form-label-lg">Last Name</label>
+            <div class="col-sm-10">
+              <input type="text" onChange={this.changeLastName} value={this.state.lastName} className='form-control' placeholder='eg. Krasinski' name='lastName'/>
+            </div>
+            {this.state.submitted && !(this.state.lastName) ? <span>Please enter a last name</span> : null}
+          </div>
+          <div class="row mb-3">
+            <label for="inputEmail" class="col-sm-2 col-form-label-lg">Email</label>
+            <div class="col-sm-10">
+              <input type="email" onChange={this.changeEmail} value={this.state.email} className='form-control' placeholder='eg. johnkrasinski@gmail.com' name='email'/>
+            </div>
+            {this.state.submitted && !(this.state.email) ? <span>Please enter an email address</span> : null}
+          </div>
+          <button type='submit' className="col-10">Register</button>
         </form>
-
       </div>
     );
   }
